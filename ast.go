29452b34fcbdb793e84f9b6b71590abf43ff7221
Rene Zhang <rz99@cornell.edu>
2014-12-27 04:43:34 -0500

    rearranging

--- ast.go
+++ ast.go
@@ -7,22 +7,19 @@ type Expr interface {
 	isExpr()
 }
 
-type Var string       // v
-func (v Var) isExpr() {}
-
+type Var string   // v
 type Abs struct { // \x.e
 	x Var
 	e Expr
 }
-
-func (e Abs) isExpr() {}
-func (e Abs) isVal()  {}
-
 type App struct { // e1 e2
 	e1 Expr
 	e2 Expr
 }
 
-func (e App) isExpr() {}
-
 type Env func(Var) Val
+
+func (v Var) isExpr() {}
+func (e Abs) isExpr() {}
+func (e Abs) isVal()  {}
+func (e App) isExpr() {}
