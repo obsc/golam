d3a9133bacffb6d51a1a7278bcfdffab9f8aad23
Rene Zhang <rz99@cornell.edu>
2015-01-03 05:27:49 -0500

    scan loop

--- lam/scan.go
+++ lam/scan.go
@@ -3,21 +3,33 @@ package lam
 import (
 	"bufio"
 	"io"
+	"strings"
 )
 
 type Scanner struct {
-	buf *bufio.Reader
+	buf   *bufio.Reader
+	Lines chan string
 }
 
 func (s *Scanner) run() {
 	for {
-
+		line, err := s.buf.ReadString('\n')
+		if err == io.EOF {
+			s.Lines <- strings.TrimRight(line, "\n")
+			break
+		} else if err == nil {
+			s.Lines <- line
+		} else {
+			panic(err)
+		}
 	}
+	close(s.Lines)
 }
 
 func ScanReader(rd io.Reader) *Scanner {
 	s := &Scanner{
-		buf: bufio.NewReader(rd),
+		buf:   bufio.NewReader(rd),
+		Lines: make(chan string),
 	}
 	go s.run()
 	return s
