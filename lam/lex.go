4d10a50ed7d38eeb64cab4218fd14ee3a770af26
Rene Zhang <rz99@cornell.edu>
2015-01-04 02:00:47 -0500

    basic lexing loop

--- lam/lex.go
+++ lam/lex.go
@@ -1,1 +1,33 @@
 package lam
+
+type itemType int
+
+type item struct {
+	typ itemType
+	val string
+}
+
+type lexer struct {
+	lines <-chan string
+	input string
+	start int
+	pos   int
+	width int
+	items chan item
+}
+
+func (l *lexer) loop() {
+	for {
+
+	}
+	close(l.items)
+}
+
+func Lexer(lines <-chan string) <-chan item {
+	l := &lexer{
+		lines: lines,
+		items: make(chan item),
+	}
+	go l.loop()
+	return l.items
+}
